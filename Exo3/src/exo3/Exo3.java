3255dc17a743c0508df33139301b3cf39d5d3888
Durandal69 <stanislas.fleutot@gmail.com>
2024-04-07 19:33:56 +0200

    Exo3-v1-2
    On va faire la bataille, et une bataille bien faite, elle se prépare avec du temps !

--- Exo3/src/exo3/Exo3.java
+++ Exo3/src/exo3/Exo3.java
@@ -4,6 +4,7 @@
  */
 package exo3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -16,10 +17,16 @@ public class Exo3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] jeudecartes;
-        jeudecartes = generationDesCartes();
+        String[] jeudecartes;   // C'est mon tableau avec mon jeu de carte. Je déclare un tableau de chaînes de caractère (String) qui s'appelle "jeudecartes"
+        jeudecartes = generationDesCartes();    // Faut lire comme en jap' (droite à gauche), la fonction generationDesCartes renvoie à un tableau de String "jeudecartes"
+        System.out.println(Arrays.toString(jeudecartes));   // Ici j'affiche toutes les valeurs du tableau
+        jeudecartes = melangeDesCartes(jeudecartes);  // Ici la fonction melangeDesCartes prend en paramètre le jeudecartes. ça veut dire que paquetDeCartes = jeudecartes et donc que leurs valeurs changent après la résolution de la fonction
         System.out.println(Arrays.toString(jeudecartes));
-        melangeDesCartes(jeudecartes);
+        ArrayList<String> mainJ1 = new ArrayList();
+        ArrayList<String> mainJ2 = new ArrayList();
+        laDistribution(mainJ1, mainJ2, jeudecartes);
+        System.out.println(mainJ1.toString());
+        System.out.println(mainJ2.toString());
     }
 
     /**
@@ -42,7 +49,7 @@ public class Exo3 {
         return jeuDeCartes;
     }
 
-    public static void melangeDesCartes(String[] paquetDeCartes) {
+    public static String[] melangeDesCartes(String[] paquetDeCartes) {
 
         String[] paquetmelange = new String[52];
         for (int carte = 0; carte < paquetDeCartes.length; carte++) {
@@ -51,19 +58,54 @@ public class Exo3 {
 
                 paquetmelange[melange] = paquetDeCartes[carte];
             } else {
-                erreurMelange(paquetDeCartes[carte], paquetmelange);    // permet l'appel et le lien avec la fonction suivante, paquetDeCartes deviendra carte et paquetmelange deviendra paquet (dans la fonction suivante)
+                paquetmelange = erreurMelange(paquetDeCartes[carte], paquetmelange);    // permet l'appel et le lien avec la fonction suivante, paquetDeCartes deviendra carte et paquetmelange deviendra paquet (dans la fonction suivante)
             }
         }
+        return paquetmelange;
     }
-    
-    public static void erreurMelange(String carte, String[] paquet) {
-        for (int indexPaquet=0; indexPaquet < paquet.length ; indexPaquet++) {
-            if (paquet[indexPaquet] != null) {
-                continue;
-            } else {
+
+    public static String[] erreurMelange(String carte, String[] paquet) {
+        for (int indexPaquet = 0; indexPaquet < paquet.length; indexPaquet++) {
+            if (paquet[indexPaquet] == null) {
                 paquet[indexPaquet] = carte;
                 break;
-            } 
+            }
+        }
+        return paquet;
+    }
+
+    public static void laDistribution(ArrayList<String> mainJ1, ArrayList<String> mainJ2, String[] paquetmelange) {
+        for (int carteJ1 = 0; carteJ1 < paquetmelange.length; carteJ1 = carteJ1 + 2) {
+            mainJ1.add(paquetmelange[carteJ1]);
+            mainJ2.add(paquetmelange[carteJ1 + 1]);
+
+        }
+    }
+
+    public static int valeurCarte(String carte) {
+        String valeur;
+        valeur = carte.split("-")[1];
+        switch (valeur) {
+            case "As":
+                return 14;
+            case "Roi":
+                return 13;
+            case "Dame":
+                return 12;
+            case "Valet":
+                return 11;
+            default:
+                return Integer.valueOf(valeur);
+
+        }
+    }
+
+    public static void leJeu(ArrayList<String> mainJ1, ArrayList<String> mainJ2) {
+        int valeurCarteJ1 = valeurCarte(mainJ1.getFirst());
+        int valeurCarteJ2 = valeurCarte(mainJ2.getFirst());
+        if (valeurCarteJ1 < valeurCarteJ2) { // on pause ici, faut maintenant add la carte de J1 dans J2 puis suppr dans J1 cette carte. Soit par fonction (merci nano) soit par des lignes de code à bouffer (mais plus facile)
+            
+            
         }
     }
 }
